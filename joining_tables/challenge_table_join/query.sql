c08983bd83d95f96de6b6d129966bb3107007b91
chiriacnicolae <>
2025-11-13 19:42:01 +0000

    Challenge: Table Join

---
+++ joining_tables/challenge_table_join/query.sql
@@ -0,0 +1,7 @@
+select 
+enrollments.enrollment_id,
+enrollments.student_name,
+enrollments.enrollment_date,
+courses.course_name,
+courses.description
+from courses  RIGHT JOIN enrollments  ON enrollments.course_id = courses.course_id;
